487c7d91f79829f388743af6f6fb1d1d63ccac4a
Patrick Haun <bomgar85@googlemail.com>
2014-08-22 17:34:59 +0200

    server is now an actor

--- airc.server/src/main/scala/net/wohey/airc/Starter.scala
+++ airc.server/src/main/scala/net/wohey/airc/Starter.scala
@@ -2,20 +2,14 @@ package net.wohey.airc
 
 import java.net.{InetSocketAddress, InetAddress}
 
-import akka.actor.ActorSystem
+import akka.actor.{Props, ActorSystem}
 import net.wohey.airc.server.IrcServer
 
 object Starter {
 
   def main(args: Array[String]): Unit = {
-    val ircServerSystem: ActorSystem = ActorSystem.create("ircserver")
-    implicit val ec = ircServerSystem.dispatcher
-
-    val serverStart = new IrcServer(system = ircServerSystem, new InetSocketAddress(9999)).start()
-
-    serverStart.onFailure {
-      case _ => ircServerSystem.shutdown()
-    }
+    val ircServerSystem = ActorSystem.create("ircserver")
+    ircServerSystem.actorOf(Props(new IrcServer(new InetSocketAddress(9999), shutdownSystemOnError = true)))
   }
 
 }
--- airc.server/src/main/scala/net/wohey/airc/server/IrcServer.scala
+++ airc.server/src/main/scala/net/wohey/airc/server/IrcServer.scala
@@ -2,10 +2,8 @@ package net.wohey.airc.server
 
 import java.net.InetSocketAddress
 
-import akka.actor.ActorSystem
-import akka.event.slf4j.SLF4JLogging
+import akka.actor.{Actor, ActorLogging, Status}
 import akka.io.IO
-import akka.pattern.ask
 import akka.stream.io.StreamTcp
 import akka.stream.io.StreamTcp.IncomingTcpConnection
 import akka.stream.scaladsl.Flow
@@ -16,11 +14,9 @@ import net.wohey.airc.parser.IrcMessageParser
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
 
-class IrcServer(system: ActorSystem, val serverAddress: InetSocketAddress) extends SLF4JLogging {
+class IrcServer(val serverAddress: InetSocketAddress, shutdownSystemOnError : Boolean = false) extends Actor with ActorLogging {
 
-  implicit val sys = system
-
-  implicit val executionContext = system.dispatcher
+  implicit val system = context.system
 
   val settings = MaterializerSettings()
 
@@ -30,24 +26,22 @@ class IrcServer(system: ActorSystem, val serverAddress: InetSocketAddress) exten
 
   val delimiter = ByteString("\r\n".getBytes("UTF-8"))
 
-  def start() = {
-    val serverFuture = IO(StreamTcp) ? StreamTcp.Bind(settings, serverAddress)
-
-    serverFuture.onSuccess {
-      case serverBinding: StreamTcp.TcpServerBinding =>
-        log.info("Server started, listening on: " + serverBinding.localAddress)
-
-        Flow(serverBinding.connectionStream).foreach { conn ⇒
-          log.info(s"Client connected from: ${conn.remoteAddress}")
-          createIncomingFlow(conn)
-        }.consume(materializer)
-    }
-
-    serverFuture.onFailure{ case e : Exception => log.error(s"Server could not bind to $serverAddress: ${e.getMessage}")}
-
-    serverFuture.map(_ => true)
+  override def preStart() = {
+    IO(StreamTcp) ! StreamTcp.Bind(settings, serverAddress)
   }
 
+  def receive: Receive = {
+    case serverBinding: StreamTcp.TcpServerBinding =>
+      log.info("Server started, listening on: " + serverBinding.localAddress)
+
+      Flow(serverBinding.connectionStream).foreach { conn ⇒
+        log.info(s"Client connected from: ${conn.remoteAddress}")
+        createIncomingFlow(conn)
+      }.consume(materializer)
+    case Status.Failure(e)  =>
+      log.error(e, s"Server could not bind to $serverAddress: ${e.getMessage}")
+      if(shutdownSystemOnError) system.shutdown()
+  }
 
   def createIncomingFlow(conn: IncomingTcpConnection) {
     val delimiterFraming = new DelimiterFraming(maxSize = 1000, delimiter = delimiter)
