20316b2a840900384cbaa929771d9ab19e78dcc3
Patrick Haun <bomgar85@googlemail.com>
2014-08-23 21:31:43 +0200

    minor refactoring

--- airc.server/src/test/scala/net/wohey/airc/user/UserTest.scala
+++ airc.server/src/test/scala/net/wohey/airc/user/UserTest.scala
@@ -8,8 +8,6 @@ import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 class UserTest(_system: ActorSystem) extends TestKit(_system)
   with ImplicitSender with WordSpecLike with Matchers with BeforeAndAfterAll {
 
-  val user = TestFSMRef(new User())
-
   def this() = {
     this(ActorSystem(classOf[UserTest].getSimpleName, ConfigFactory.parseString("ircserver.password=test")))
   }
@@ -21,12 +19,14 @@ class UserTest(_system: ActorSystem) extends TestKit(_system)
   "An User actor" should {
 
     "reject invalid passwords" in {
+      val user = TestFSMRef(new User())
       user ! User.Authenticate(password = "moep")
       expectMsg(User.InvalidPassword)
       user.stateName == User.AuhenticationPending
     }
 
     "accept valid passwords" in {
+      val user = TestFSMRef(new User())
       user ! User.Authenticate(password = "test")
       user.stateName == User.RegistrationPending
     }
